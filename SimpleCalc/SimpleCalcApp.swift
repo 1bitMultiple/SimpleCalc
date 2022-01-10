8bfe8f19a4037a5ba5fafafea7832cfc62b2517a
Takayuki Kamezawa <kamezawa.takayuki@roomclip.jp>
2022-01-10 14:44:59 +0900

    :art: first commit create new project

---
+++ SimpleCalc/SimpleCalcApp.swift
@@ -0,0 +1,17 @@
+//
+//  SimpleCalcApp.swift
+//  SimpleCalc
+//
+//  Created by kamezawa.takayuki on 2022/01/10.
+//
+
+import SwiftUI
+
+@main
+struct SimpleCalcApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
